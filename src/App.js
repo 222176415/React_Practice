841486524de40b62952d7ae508b392d0508b29ec
Themba  Ntimane <141935793+222176415@users.noreply.github.com>
2025-01-20 18:28:28 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./styles.css";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App(props) {
   return (
     <div className="App">
-      <fieldset>
-        <h1>Hello CodeSandbox</h1>
-        <h2 style={{ color: props.color }}>
-          Start editing to see some magic happen!
-        </h2>
-      </fieldset>
+      <p>Themba Ntimane </p>
     </div>
   );
 }
